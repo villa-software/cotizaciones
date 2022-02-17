411e959d398f8c5317bb73e3c81aa8fe78987847
Valter Junior <valterjunior@Valters-MacBook-Pro.local>
2022-02-17 09:24:25 -0300

    add fix to call correct api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
 import type { NextPage } from "next";
-
 import HomePage from "../components/Home";
 
 
 export async function getServerSideProps() {
-  const jsonData = await fetch('http://localhost:3000/api/quotes');
+  const jsonData = await fetch(process.env.NODE_ENV === 'production' ? 'https://cotizacionespy.vercel.app/api/quotes' : 'http://localhost:3000/api/quotes');
   const data = await jsonData.json();
 
   return {
