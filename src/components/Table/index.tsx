d472b7fd277ea5bdca139e81d6343520ec443ac8
Valter Junior <valterjunior@Valters-MacBook-Pro.local>
2022-02-18 17:27:37 -0300

    add table

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,65 +1,33 @@
-import { useMemo } from "react";
 import { Quote } from "src/types";
-import styled from "styled-components";
 import { Table } from 'antd';
-
-type ColumnDefinitionType = {
-  id: string;
-  title: string;
-};
-
 interface Props {
   data: Quote[];
 }
 
-const TableWrap = styled.div`
-    max-width: 100%;
-    width: 100%;
-`;
-
-const TableWithStyle = styled.table`
-    width: 100%;
-`;
+const columns = [
+  {
+    title: 'Casa de Câmbio',
+    key: 'name',
+    render: (row: Quote) => {
+      return (<span>{row.office.name}</span>)
+    }
+  },
+  {
+    title: 'Compra',
+    key: 'age',
+    render: (row: Quote) => {
+      return (<span>{row.usd.purchasePrice}</span>)
+    }
+  },
+  {
+    title: 'Venda',
+    key: 'address',
+    render: (row: Quote) => {
+      return (<span>{row.usd.purchasePrice}</span>)
+    }
+  },
+];
 
 export const MyTable = ({ data }: Props) => {
-  const dataSource = [
-    {
-      key: '1',
-      name: 'Mike',
-      age: 32,
-      address: '10 Downing Street',
-    },
-    {
-      key: '2',
-      name: 'John',
-      age: 42,
-      address: '10 Downing Street',
-    },
-  ];
-  
-  const columns = [
-    {
-      title: 'Casa de Câmbio',
-      key: 'name',
-      render: (row: Quote) => {
-        return (<span>{row.office.name}</span>)
-      }
-    },
-    {
-      title: 'Compra',
-      key: 'age',
-      render: (row: Quote) => {
-        return (<span>{row.usd.purchasePrice}</span>)
-      }
-    },
-    {
-      title: 'Venda',
-      key: 'address',
-      render: (row: Quote) => {
-        return (<span>{row.usd.purchasePrice}</span>)
-      }
-    },
-  ];
-  
   return <Table dataSource={data} columns={columns} />;
 };
